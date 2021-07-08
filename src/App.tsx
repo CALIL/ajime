0f96b698a574cf49dd6a541345cc67d57c23a7cc
deguchi <deguchik@gmail.com>
2021-07-08 12:01:25 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import JsBarcode from 'jsbarcode'
 
 interface App {
   canvasDiv: HTMLDivElement | null
+  startNumber: HTMLInputElement | null
+  endNumber: HTMLInputElement | null
 }
 
 
@@ -20,13 +22,30 @@ class App extends Component<Props, State> {
       this.state = {
       }
       this.canvasDiv = null
+      this.startNumber = null
+      this.endNumber = null
   }
   componentDidMount() {
-    this.renderBarcode('123456789')
+    // this.renderBarcode('123456789')
+    this.renderBarCodes()
   }
-  renderBarcode(number: string) {
+  renderBarCodes() {
+    if (this.startNumber && this.endNumber) {
+      const startNumber = parseInt(this.startNumber.value)
+      const endNumber = parseInt(this.endNumber.value)
+      console.log(startNumber, endNumber)
+      if (startNumber >= endNumber) return console.log('数字の指定が間違っています')
+      let currentNumber = startNumber
+      while(currentNumber <= endNumber) {
+        console.log(currentNumber)
+        this.renderBarcode(currentNumber)
+        currentNumber += 1
+      }
+    }
+  }
+  renderBarcode(number: number) {
     const canvas = document.createElement('canvas')
-    JsBarcode(canvas, number, {
+    JsBarcode(canvas, String(number), {
       format: 'codabar'
     })
     if (this.canvasDiv) {
@@ -36,6 +55,17 @@ class App extends Component<Props, State> {
   render() {
     return (
       <div className="App">
+        <header>
+          <div>
+            <label htmlFor=""></label>
+            <input type="number" defaultValue="10000" ref={element => this.startNumber = element} />
+            -
+            <label htmlFor=""></label>
+            <input type="number" defaultValue="11000" ref={element => this.endNumber = element} />
+            <button onClick={this.renderBarCodes.bind(this)}>バーコード作成</button>
+          </div>
+          <button onClick={print}>印刷</button>
+        </header>
         <div ref={element => this.canvasDiv = element}></div>
       </div>
     )  
