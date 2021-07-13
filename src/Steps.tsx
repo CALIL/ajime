bfcf2eb09c0f6dc3d2cab32d6b48d935c2dcb1e1
deguchi <deguchik@gmail.com>
2021-07-13 14:02:54 +0900

    update

--- src/Steps.tsx
+++ src/Steps.tsx
@@ -12,7 +12,7 @@ export const Step1 = (props: any) => {
     return (
         <div className="step step1">
             <h2>Step {props.currentStep}</h2>
-            <p>印刷に使うタックシールを選んでください</p>
+            <p>印刷に使うラベルシールを選んでください</p>
             <ul>
                 <li>
                     <input type="radio" name="template" id="AONE" value="AONE" defaultChecked={true} onChange={(e) => props.onSelectTemplate(e.target.value)} />
