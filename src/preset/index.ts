8464394c391a9fc4546e52989d3fab4444e87dec
deguchi <deguchik@gmail.com>
2021-07-16 11:44:23 +0900

    update

--- src/preset/index.ts
+++ src/preset/index.ts
@@ -1,11 +1,13 @@
 import aone28368 from  './aone-28368.json'
 import kihara606906 from  './kihara-606906.json'
 import askul56249 from  './askul-56249.json'
+import aone72295 from  './aone-72295.json'
 
 const preset : any = {
     'aone-28368': aone28368,
     'kihara-606906': kihara606906,
-    'askul-56249': askul56249
+    'askul-56249': askul56249,
+    'aone-72295': aone72295
 }
 
 export default preset
