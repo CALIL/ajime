57ebb63c6aae2dce39d6107875c0c9d761ea15d2
deguchi <deguchik@gmail.com>
2021-07-19 15:21:09 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -202,6 +202,9 @@ class App extends Component<Props, State> {
             </section>)
           })}
         </div>
+        <footer>
+          <span className="poweredby"></span>
+        </footer>
       </div>
     )
   }
